97f357c636c3e887f2e7a6a62b9876d9c16ca6fd
JuliePro <prokofevajuliya@gmail.com>
2024-10-22 11:58:16 +0300

    added option and argument

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -3,7 +3,8 @@
 const { program } = require('commander');
 
 program
-    .version('1.0.0') 
-    .description('Compares two configuration files and shows a difference.');
-
-program.parse(process.argv);
+    .version('1.0.0')
+    .argument('<filepath1> <filepath2>')
+    .description('Compares two configuration files and shows a difference.')
+    .option('-f, --format [type]', 'output format')
+    .parse(process.argv);
